1cff5c8db43fa3fb843a4b7db8f2d9bdecf34988
Avalanche322 <avalfnche@gmail.com>
2025-07-14 09:28:54 +0300

    fix: Handle geolocation errors and dispatch notifications in CityProvider

--- src/context/CityProvider.tsx
+++ src/context/CityProvider.tsx
@@ -1,17 +1,28 @@
 import { useEffect, useState, type ReactNode } from "react";
 import { CityContext } from "./CityContext";
+import { showNotification } from "../features/notification/slice";
+import { useDispatch } from "react-redux";
 
 export function CityProvider({ children }: { children: ReactNode }) {
   const [city, setCity] = useState<string>("");
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!navigator.geolocation) return;
 
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setCity(`${latitude},${longitude}`);
-    });
-  }, []);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setCity(`${latitude},${longitude}`);
+      },
+      (error) => {
+        console.warn("Geolocation error:", error.message);
+        dispatch(
+          showNotification({ message: error.message, severity: "error" })
+        );
+      }
+    );
+  }, [dispatch]);
 
   return (
     <CityContext.Provider value={{ city, setCity }}>
